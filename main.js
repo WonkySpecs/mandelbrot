9a7b2190767ba2849f958d91d6ab3998a3a39cb6
Will Taylor <w1ll100@hotmail.co.uk>
2019-02-09 23:08:11 +0000

    Click to set center coordinates

--- main.js
+++ main.js
@@ -1,25 +1,10 @@
 import * as drawing from "./modules/drawing.js";
 import * as mandelbrot from "./modules/mandelbrot.js";
 
-centerXInput.value = -0.5;
-centerYInput.value = 0;
-zoomInput.value = 1;
 const originalWidth = 3;
 const originalHeight = 2;
 
-redrawBtn.onclick = function() {
-	redraw(calcSizesFromInputs());
-};
-
-function calcSizesFromInputs() {
-	let cx = parseFloat(centerXInput.value);
-	let cy = parseFloat(centerYInput.value);
-	let width = originalWidth / parseFloat(zoomInput.value);
-	let height = originalHeight / parseFloat(zoomInput.value);
-	return [cx - width / 2, cx + width / 2, cy - height / 2, cy + height / 2];
-}
-
-function redraw([minX, maxX, minY, maxY]) {
+function redraw(minX, maxX, minY, maxY) {
 	const canvas = document.getElementById('canvas');
 	const scaledPoints = drawing.scalePixelListToAxes([canvas.width, canvas.height], [minX, maxX, minY, maxY]);
 	let maxIterations = 50;
@@ -28,3 +13,47 @@ function redraw([minX, maxX, minY, maxY]) {
 	const pointColours = drawing.ColourMapper('smooth').apply(escapeTimes, maxIterations);
 	drawing.drawPoints(pointColours, canvas);
 }
+
+var Inputs = function() {
+	let calcSizesFromInputs = function() {
+		let cx = parseFloat(centerXInput.value);
+		let cy = parseFloat(centerYInput.value);
+		let width = originalWidth / parseFloat(zoomInput.value);
+		let height = originalHeight / parseFloat(zoomInput.value);
+		return [cx - width / 2, cx + width / 2, cy - height / 2, cy + height / 2];
+	}
+
+	let [axesLeft, axesRight, axesTop, axesBottom] = calcSizesFromInputs();
+
+	let bindEvents = function() {
+		redrawBtn.onclick = function() {
+			[axesLeft, axesRight, axesTop, axesBottom] = calcSizesFromInputs();
+			redraw(axesLeft, axesRight, axesTop, axesBottom);
+		};
+
+		canvas.onmousemove = function(e) {
+			let canvas = document.getElementById('canvas');
+			let canvasRect = canvas.getBoundingClientRect();
+			let mouseX = e.clientX - canvasRect.left;
+			let mouseY = e.clientY - canvasRect.top;
+			canvasMouseX.value = axesLeft + mouseX / canvas.width * (axesRight - axesLeft);
+			canvasMouseY.value = axesBottom + mouseY / canvas.height * (axesTop - axesBottom);
+		}
+
+		canvas.onclick = function(e) {
+			centerXInput.value = canvasMouseX.value;
+			centerYInput.value = canvasMouseY.value;
+		}
+	}
+	return {
+		initialize: function() {
+			centerXInput.value = -0.5;
+			centerYInput.value = 0;
+			zoomInput.value = 1;
+			bindEvents();
+		}
+	}
+}();
+
+Inputs.initialize();
+redrawBtn.click();
