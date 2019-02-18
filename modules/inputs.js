ff3ff15f80c271ee5a469bf0c712fde0a4b27374
Will Taylor <w1ll100@hotmail.co.uk>
2019-02-18 17:04:17 +0000

    Encapsulate colour input functions in object

--- modules/inputs.js
+++ modules/inputs.js
@@ -14,14 +14,90 @@ let calcSizesFromInputs = function() {
 }
 
 let [axesLeft, axesRight, axesTop, axesBottom] = calcSizesFromInputs();
-let colourInputs = [document.getElementById("noEscapeColour"),
-					document.getElementById("latestEscapeColour"),
-					document.getElementById("earliestEscapeColour")];
+
+let ColourInputHandler = function() {
+	const MAX_COLOURS = 10
+	const MIN_COLOURS = 3;
+	let colourInputControls = [document.getElementById("noEscapeColour"),
+							   document.getElementById("latestEscapeColour"),
+							   document.getElementById("earliestEscapeColour")];
+	return {
+		newColourEvent: function(callback) {
+			if(colourInputControls.length < MAX_COLOURS) {
+				let newControl = buildColourInput(colourInputControls.length, callback);
+				const controlDiv = document.getElementById("colourControls");
+				controlDiv.insertBefore(newControl, earliestEscapeColour);
+				renameColourInputs();
+				colourInputControls.push(newControl);
+			}
+		},
+		calculateColours: calculateColours
+	}
+
+	function buildColourInput(inputNumber, callback) {
+		let label = document.createElement("label");
+		label.innerHTML = "Colour " + inputNumber + ":";
+
+		let input = document.createElement("input");
+		input.type = "color";
+		input.onchange = function(e) {
+			callback(calculateColours());
+		}
+
+		let deleteBtn = document.createElement("button");
+		deleteBtn.innerHTML = "-";
+
+		const parent = document.createElement("span")
+		parent.id = "colour" + inputNumber;
+		parent.appendChild(label);
+		parent.appendChild(input);
+		parent.appendChild(deleteBtn);
+
+		deleteBtn.onclick = function() {
+			removeColourInput(parent);
+			renameColourInputs();
+			callback(calculateColours());
+			parent.remove();
+		};
+		return parent;
+	}
+
+	function removeColourInput(toRemove) {
+		let toRemoveIndex;
+		for(let i = MIN_COLOURS; i < colourInputControls.length; i++) {
+			if(colourInputControls[i].id === toRemove.id) {
+				toRemoveIndex = i;
+				break;
+			}
+		}
+		if(toRemoveIndex != -1) {
+			colourInputControls.splice(toRemoveIndex, 1);
+		} else {
+			throw "Could not find colour input " + toRemove;
+		}
+	}
+
+	function renameColourInputs() {
+		for(let i = MIN_COLOURS; i < colourInputControls.length; i++) {
+			let control = colourInputControls[i];
+			control.id = "colour" + i;
+			let label = control.getElementsByTagName("label")[0];
+			label.innerHTML = "Colour " + i + ":";
+		}
+	}
+
+	function calculateColours() {
+		return colourInputControls.map(function(control) {
+			let input = control.getElementsByTagName("input")[0];
+			return hexStringToRgb(input.value);
+		});
+	}
+}();
 
 let bindEvents = function(main) {
 	redrawBtn.onclick = function() {
 		[axesLeft, axesRight, axesTop, axesBottom] = calcSizesFromInputs();
-		main.rerender([axesLeft, axesRight, axesTop, axesBottom], calculateColours());
+		main.rerender([axesLeft, axesRight, axesTop, axesBottom], ColourInputHandler.calculateColours());
 	};
 
 	canvas.onmousemove = function(e) {
@@ -41,19 +117,11 @@ let bindEvents = function(main) {
 	const colourInputElements = document.querySelectorAll('input[type="color"]');
 	colourInputElements.forEach(function(element) {
 		element.onchange = function(event) {
-			main.colourAndDraw(calculateColours());
+			main.colourAndDraw(ColourInputHandler.calculateColours());
 		}
 	});
 
-	addColourBtn.onclick = function(e) {
-		if(colourInputs.length < 10) {
-			let newControl = buildColourInput(colourInputs.length, main.colourAndDraw);
-			const controlDiv = document.getElementById("colourControls");
-			controlDiv.insertBefore(newControl, earliestEscapeColour);
-			renameColourInputs();
-			colourInputs.push(newControl);
-		}
-	}
+	addColourBtn.onclick = (() => ColourInputHandler.newColourEvent(main.colourAndDraw));
 }
 
 function initialize(main) {
@@ -61,62 +129,4 @@ function initialize(main) {
 	centerYInput.value = 0;
 	zoomInput.value = 1;
 	bindEvents(main);
-}
-
-function buildColourInput(inputNumber, callback) {
-	let label = document.createElement("label");
-	label.innerHTML = "Colour " + inputNumber + ":";
-
-	let input = document.createElement("input");
-	input.type = "color";
-	input.onchange = function(e) {
-		callback(calculateColours());
-	}
-
-	let deleteBtn = document.createElement("button");
-	deleteBtn.innerHTML = "-";
-
-	const parent = document.createElement("span")
-	parent.id = "colourInput" + inputNumber;
-	parent.appendChild(label);
-	parent.appendChild(input);
-	parent.appendChild(deleteBtn);
-
-	deleteBtn.onclick = function() {
-		removeColourInput(parent);
-		renameColourInputs()
-		parent.remove();
-	};
-	return parent;
-}
-
-function removeColourInput(toRemove) {
-	let toRemoveIndex;
-	for(let i = 3; i < colourInputs.length; i++) {
-		if(colourInputs[i].id === toRemove.id) {
-			toRemoveIndex = i;
-			break;
-		}
-	}
-	if(toRemoveIndex != -1) {
-		colourInputs.splice(toRemoveIndex, 1);
-	} else {
-		throw "Could not find colour input " + toRemove;
-	}
-}
-
-function renameColourInputs() {
-	for(let i = 3; i < colourInputs.length; i++) {
-		let control = colourInputs[i];
-		control.id = "colourInput" + i;
-		let label = control.getElementsByTagName("label")[0];
-		label.innerHTML = "Colour " + i + ":";
-	}
-}
-
-function calculateColours() {
-	return colourInputs.map(function(control) {
-		let input = control.getElementsByTagName("input")[0];
-		return hexStringToRgb(input.value);
-	});
 }
